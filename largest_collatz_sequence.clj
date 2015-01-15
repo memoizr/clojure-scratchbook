08bb2dcd3fdf31752c8f01e95df7a859ac95422a
memoizr <memoizrlabs@gmail.com>
2015-01-15 19:44:59 +0000

    add alternative solution to p 14

--- largest_collatz_sequence.clj
+++ largest_collatz_sequence.clj
@@ -1,3 +1,6 @@
+
+
+
 (defn collatz [n]
   ((fn [x acc]
     (cond
@@ -5,6 +8,43 @@
           (zero? (rem x 2))
           (recur (/ x 2) (cons x acc))
           :else (recur (inc (* 3 x)) (cons x acc)))) n ()))
+
+(defn build-list1 [n]
+  (loop [i 1 acc 0 result 0]
+    (cond
+      (= i n) [result acc]
+      (> (collatz i) acc) (recur (inc i) (collatz i) i)
+      :else (recur (inc i) acc result))))
+
+;(println (build-list1 10000))
+
+
+
+;================================
+;================================
+
+; Sombody else's implementation from the forums.
+; It is also correct but it runs about 4 times slower.
+
+(defn collatz1 [start]
+  (cons start ((defn sub-collatz [n]
+                 (cond (= 1 n) nil
+                       (zero? (mod n 2)) (cons (/ n 2) (lazy-seq (sub-collatz (/ n 2))))
+                 :else (cons (inc (* n 3)) (lazy-seq (sub-collatz (inc (* n 3)))))))
+               start)))
+
+(defn f
+  ([c] c)
+  ([c1 c2] (if (> (count c1) (count c2)) c1 c2)))
+
+(time (build-list1 1000000))
+(time (first (reduce f (map collatz1 (range 2 1000001)))))
+(time (build-list1 1000000))
+(time (first (reduce f (map collatz1 (range 2 1000001)))))
+
+
+;================================
+;================================
 
 ;(defn build-list [n]
   ;(into (sorted-set)
@@ -24,12 +64,4 @@
       acc
       (recur (inc i) (list (apply max (cons (collatz i) acc)))))))
 
-(defn build-list1 [n]
-  (loop [i 1 acc 0 result 0]
-    (cond
-      (= i n) [result acc]
-      (> (collatz i) acc) (recur (inc i) (collatz i) i)
-      :else (recur (inc i) acc result))))
-
-(time (build-list1 1000000))
 
